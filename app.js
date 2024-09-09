5d057b0512307fd8ec8b44b5b953320d516d0c6b
Andrzej Słomka <andrzejslomkapraca@gmail.com>
2024-09-09 23:25:10 +0200

    Added base auth

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const logger = require('morgan')
 const cors = require('cors')
 
 const contactsRouter = require('./routes/api/contacts')
+const { userRouter } = require('./routes/api/user');
+const passport = require('./config/config-passport.js');
 
 const app = express()
 
@@ -13,7 +15,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/contacts', contactsRouter)
+app.use('/api/contacts', contactsRouter);
+app.use('/api/users', userRouter);
+app.use(passport.initialize());
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
